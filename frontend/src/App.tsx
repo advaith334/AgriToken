cfc88007309eb623b6cbabf88c697cb2f740e17b
Lee Sungmin <luckyisland3710@gmail.com>
2025-09-06 19:17:22 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true
+        }}
+      >
         <Navbar />
         <Routes>
           <Route path="/" element={<Landing />} />
