551c49138a462387789fdf45cbcb1dd2ea6f0400
Advaith Karthikeyan <advaith334@gmail.com>
2025-09-06 16:43:35 -0400

    Resetting backend

--- backend/projects/backend/smart_contracts/tokenize_farm/deploy_config.py
+++ backend/projects/backend/smart_contracts/tokenize_farm/deploy_config.py
@@ -7,16 +7,16 @@ logger = logging.getLogger(__name__)
 
 # define deployment behaviour based on supplied app spec
 def deploy() -> None:
-    from smart_contracts.artifacts.agritoken_backend.agritoken_backend_client import (
+    from smart_contracts.artifacts.tokenize_farm.tokenize_farm_client import (
         HelloArgs,
-        AgritokenBackendFactory,
+        TokenizeFarmFactory,
     )
 
     algorand = algokit_utils.AlgorandClient.from_environment()
     deployer_ = algorand.account.from_environment("DEPLOYER")
 
     factory = algorand.client.get_typed_app_factory(
-        AgritokenBackendFactory, default_sender=deployer_.address
+        TokenizeFarmFactory, default_sender=deployer_.address
     )
 
     app_client, result = factory.deploy(
